782b08e562a04a80a26c9702ca3e04e9549e0bc1
Thales Ribeiro <thalessr@gmail.com>
2024-03-19 16:24:55 -0300

    Add stalkerware's list generation

--- generate_lists.py
+++ generate_lists.py
@@ -21,12 +21,26 @@ def generate_ad_block_file() -> None:
             if (line.startswith('||')):
                 url = line.replace('||', "")
                 url = url.replace('^', "")
-                file.write(f"add list=adblocker comment=AdGuardHome address={url}\n")
+                file.write(f"add list=adblocker comment=AdGuardHome-ADblocker address={url}\n")
+
+
+def generate_stalkerware_list() -> None:
+    response = urlopen(
+        'https://raw.githubusercontent.com/AssoEchap/stalkerware-indicators/master/generated/hosts').read()
+    decoded_response = response.decode('utf-8')
+    stalkerware_list = decoded_response.split("\n")
+    with open("dist/stalkerware.rsc", "w") as file:
+        file.write("# ============================================================\n")
+        file.write("# Last Update     : " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + "\n")
+        file.write("/ip firewall address-list \n")
+        for line in stalkerware_list:
+            file.write(f"add list=stalkerware comment=AdGuardHome-Stalkerware address={line}\n")
 
 
 async def main() -> None:
     generate_lists_folder()
     generate_ad_block_file()
+    generate_stalkerware_list()
 
 
 if __name__ == "__main__":
